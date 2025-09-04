3e5604d0881591de6e25f00040cc6ad8f953b5c5
Kamil Jan Mularski <kammul@outlook.com>
2025-09-04 20:47:04 +0200

    The first solution

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,16 @@
 package core.basesyntax;
 
 //write your code here
+public class PasswordValidationException extends Exception {
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,20 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator validator = new PasswordValidator();
+
     public void registerUser(User user) {
         //write your code here
+        if (user == null) {
+            System.out.println("User cannot be null");
+            return;
+        }
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
